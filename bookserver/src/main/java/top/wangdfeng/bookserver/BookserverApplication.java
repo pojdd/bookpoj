4d4c768078a8a34211160b50b515d203df6e0820
wdfHPY <43898347+wdfHPY@users.noreply.github.com>
2020-01-11 10:05:01 +0800

    初始化springboot服务端
    创建了bookserver。一个完全的空的springboot项目

---
+++ bookserver/src/main/java/top/wangdfeng/bookserver/BookserverApplication.java
@@ -0,0 +1,13 @@
+package top.wangdfeng.bookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookserverApplication.class, args);
+    }
+
+}
