9ab18461e8fc0f77d56de61be36c96e484dfefd6
wdfHPY <43898347+wdfHPY@users.noreply.github.com>
2020-01-12 11:40:56 +0800

    书城具体类别接口完成
        * bookstore_category_fantsy
        *       玄幻接口
        * bookstore_category_city
        *       都市接口
        * bookstore_category_self-cultivation
        *       修真接口
        * bookstore_category_romance
        *        言情接口
        * bookstore_category_girl
        *        女生接口
        * bookstore_category_crossing
        *        穿越接口
        * bookstore_category_online-game
        *        网游接口
        * bookstore_category_sci-fi
        *        科幻接口

--- bookserver/src/main/java/top/wangdfeng/bookserver/service/BookService.java
+++ bookserver/src/main/java/top/wangdfeng/bookserver/service/BookService.java
@@ -9,7 +9,21 @@ public interface BookService {
     /*
     *  获取全部book表中的数据。该数据是raw数据。没有经过分类过滤的原始数据。
     * */
-    public List<Book> getBookList();
+     List<Book> getBookList();
 
-    public List<Book> getBookListFantsy();
+     List<Book> getBookListFantsy();
+
+     List<Book> getBookListCity();
+
+     List<Book> getBookListSelfCultivation();
+
+     List<Book> getBookListRomance();
+
+     List<Book> getBookListGirl();
+
+     List<Book> getBookListCrossing();
+
+     List<Book> getBookListOnlineGame();
+
+     List<Book> getBookListSciFi();
 }
--- bookserver/src/main/java/top/wangdfeng/bookserver/service/impl/BookServiceImpl.java
+++ bookserver/src/main/java/top/wangdfeng/bookserver/service/impl/BookServiceImpl.java
@@ -33,4 +33,95 @@ public class BookServiceImpl implements BookService {
         }
         return newbooks;
     }
+
+    @Override
+    public List<Book> getBookListCity() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[都市]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
+
+    @Override
+    public List<Book> getBookListSelfCultivation() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[修真]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
+
+    @Override
+    public List<Book> getBookListRomance() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[言情]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
+
+    @Override
+    public List<Book> getBookListGirl() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[女生]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
+
+    @Override
+    public List<Book> getBookListCrossing() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[穿越]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
+
+    @Override
+    public List<Book> getBookListOnlineGame() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[网游]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
+
+    @Override
+    public List<Book> getBookListSciFi() {
+        List<Book> newbooks=new ArrayList<>();
+        List<Book> books=getBookList();
+        for(Book book:books){
+            String string=book.getBookclass();
+            if(string.equals("[科幻]")){
+                newbooks.add(book);
+            }
+        }
+        return newbooks;
+    }
 }
