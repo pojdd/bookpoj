a761214195365edbe6599d32c8ba2fb677a99728
wdfHPY <43898347+wdfHPY@users.noreply.github.com>
2020-01-12 15:45:47 +0800

    wx客户端界面完成
    接口测试完成
    点击事件监听尚未完成

--- wxnovel/pages/bookstore/bookstore.js
+++ wxnovel/pages/bookstore/bookstore.js
@@ -5,14 +5,148 @@ Page({
    * 页面的初始数据
    */
   data: {
-    background: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1XNvasoQ0GU1S1JdL8t_IiuoIkzkFdGLjeXrehJxZekcqSuDuNQ&s","http://pic.51yuansu.com/backgd/cover/00/02/30/5b55a7417869f.jpg!/fw/780/quality/90/unsharp/true/compress/true"]
+    background: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1XNvasoQ0GU1S1JdL8t_IiuoIkzkFdGLjeXrehJxZekcqSuDuNQ&s","http://pic.51yuansu.com/backgd/cover/00/02/30/5b55a7417869f.jpg!/fw/780/quality/90/unsharp/true/compress/true"],
+    fantsy:[],
+    city:[],
+    selfcultivation:[],
+    girl:[],
+    crossing:[],
+    onlinegame:[],
+    scifi:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getFantsyData()
+    this.getCityData()
+    this.getSelfcultivationData()
+    this.getGirlData()
+    this.getCrossingData()
+    this.getOnlinegameData()
+    this.getScifiData()
+  },
 
+    /*
+    * bookstore_category_fantsy
+    * bookstore_category_city
+    * bookstore_category_self-cultivation
+    * bookstore_category_romance
+    * bookstore_category_girl
+    * bookstore_category_crossing
+    * bookstore_category_online-game
+    * bookstore_category_sci-fi
+    * */
+
+    /**
+     * getFantsyData use for get fantsy book
+     * 
+     */
+  getFantsyData:function(){
+     let that=this;
+     wx.request({
+       url: 'http://localhost:8080/bookstore_category_fantsy',
+       success:function(res){
+          that.setData({
+            fantsy:res.data
+          })
+       }
+     })
+  },
+
+    /**
+     * getCityData use for get city book
+     * 
+     */
+  getCityData: function () {
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8080/bookstore_category_city',
+      success: function (res) {
+        that.setData({
+          city: res.data
+        })
+      }
+    })
+  },
+
+  /**
+ * getSelfcultivationData use for get self-cultivation book
+ */
+  getSelfcultivationData: function () {
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8080/bookstore_category_self-cultivation',
+      success: function (res) {
+        that.setData({
+          selfcultivation: res.data
+        })
+      }
+    })
+  },
+
+
+  /**
+* getGirlData use for get girl book
+*/
+  getGirlData: function () {
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8080/bookstore_category_girl',
+      success: function (res) {
+        that.setData({
+          girl: res.data
+        })
+      }
+    })
+  },
+
+  /**
+* getCrossingData use for get crossing book
+*/
+  getCrossingData: function () {
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8080/bookstore_category_crossing',
+      success: function (res) {
+        that.setData({
+          crossing: res.data
+        })
+      }
+    })
+  },
+
+
+  /**
+* getOnlinegameData use for get onlinegame book
+*/
+  getOnlinegameData: function () {
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8080/bookstore_category_online-game',
+      success: function (res) {
+        that.setData({
+          onlinegame: res.data
+        })
+      }
+    })
+  },
+
+
+  /**
+* getScifiData use for get scifi book
+*/
+  getScifiData: function () {
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8080/bookstore_category_sci-fi',
+      success: function (res) {
+        that.setData({
+          scifi: res.data
+        })
+      }
+    })
   },
 
   /**
